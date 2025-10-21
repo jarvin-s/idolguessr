0a33d1feab968f353d8667d4dd2102234626441a
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-10-21 13:08:47 +0200

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/next'
 
 const proximaNovaRegular = localFont({
     src: '../../public/fonts/proximanova_regular.ttf',
@@ -24,6 +25,7 @@ export default function RootLayout({
                 className={`${proximaNovaRegular.className} antialiased`}
             >
                 {children}
+                <Analytics />
             </body>
         </html>
     )
